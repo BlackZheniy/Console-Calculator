b9c53f3f46da1cb687e17dadd5778e2b07551f73
Євгеній Кулаковскій <142673265+BlackZheniy@users.noreply.github.com>
2024-01-03 17:08:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+number1 = int(input("Ввведть перше число: "))
+
+number2 = int(input("Ввведть друге число: "))
+
+print("Result:", number1 + number2)
+print("Result:", number1 - number2)
+print("Result:", number1 / number2)
+print("Result:", number1 * number2)
+
+word = "Thanks"
+print(word)
+
+word = True
